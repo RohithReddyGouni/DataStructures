8618b966a38b6ea4ab8d8ba9e522e0f3241deb72
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-07-14 15:44:21 -0400

    Two ways of doing the search

--- Search_an_element_in_sorted_2D_matrix.py
+++ Search_an_element_in_sorted_2D_matrix.py
@@ -14,3 +14,34 @@ class Solution:
                 else:
                     row=row+1;
         return False;
+
+
+
+
+
+// Search an element in a sorted 2D matrix
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        high = m*n-1
+        
+        while low <= high:
+            mid = low + (high - low)//2
+            r = mid//n
+            c = mid%n
+            #print(matrix[r][c])
+            
+            if matrix[r][c] == target:
+                return True
+            
+            elif matrix[r][c] > target:
+                high = mid -1
+                
+            else:
+                low = mid + 1
+                
+        return False
+                
