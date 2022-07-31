815237760abe375be1c8e25d660a3ace3988e8b7
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-07-31 13:07:08 -0400

    Ugly Number Code Added

---
+++ ugly number.py
@@ -0,0 +1,10 @@
+#Time Complexity: O(log2(n))   --> Considering n is divisible by 2 
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n<=0:
+            return False;
+        for i in [2,3,5]:
+            while n%i==0:
+                n//=i;
+        return n==1;
